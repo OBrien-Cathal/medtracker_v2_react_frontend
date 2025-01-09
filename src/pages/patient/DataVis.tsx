edf82aa2f10cc06a24d7e114284a1d8e315eb27c
OBrien-Cathal <obrien.cathal@proton.me>
2025-01-09 17:56:24 +0100

    Added react-google-charts, implemented basic systole graph on DataVis page

--- src/pages/patient/DataVis.tsx
+++ src/pages/patient/DataVis.tsx
@@ -2,23 +2,25 @@ import {useAuth} from "../../auth/AuthProvider.tsx";
 import {useEffect, useState} from "preact/compat";
 
 import {BloodPressureService} from "../../service/bloodPressure.service.tsx";
-import {IGraphData} from "../../types/generic-graph-data.type.ts";
+
+import {Chart} from "react-google-charts";
 
 const DataVis = () => {
     const {token} = useAuth()
-
-
     const bloodPressureService = new BloodPressureService(token)
-    const [graphData, setGraphData] = useState<IGraphData>()
-
+    const [data, setData] = useState<any[][]>()
 
     function getSystoleGraphData() {
         bloodPressureService.getSystoleGraphData()
             .then(r => {
-                setGraphData(r.data.graphData)
-                console.log(r.data)
+                console.log("data")
+                console.log(r.data.graphData)
+                let tempArray: any[][] = []
+                tempArray=tempArray.concat([r.data.graphData.columnNames])
+                tempArray=tempArray.concat(r.data.graphData.dataRows)
+                setData(tempArray)
             }).catch((reason) => {
-                console.log(reason.errors)
+            console.log(reason)
         });
     }
 
@@ -33,7 +35,13 @@ const DataVis = () => {
                 <div>Data</div>
             </div>
             <div>Systole</div>
-            <div>GRAPH here {graphData}</div>
+            <Chart
+                chartType={'LineChart'}
+                data={data}
+                width={"100%"}
+                height={"400px"}
+            >
+            </Chart>
         </div>
     )
 }
