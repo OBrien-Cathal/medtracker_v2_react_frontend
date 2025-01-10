7073acece41fea953edbc2b203790e32318a81fd
OBrien-Cathal <obrien.cathal@proton.me>
2025-01-10 16:11:50 +0100

    added dose graph for practitioner view

--- src/pages/Practitioner/PatientDataVis.tsx
+++ src/pages/Practitioner/PatientDataVis.tsx
@@ -1,6 +1,8 @@
 import {useAuth} from "../../auth/AuthProvider.tsx";
 import {BloodPressureService} from "../../service/bloodPressure.service.tsx";
 import SystoleGraph from "../components/graphs/SystoleGraph.tsx";
+import DoseGraph from "../components/graphs/DoseGraph.tsx";
+import {DosesService} from "../../service/dosesService.tsx";
 
 type PropsType = {
     patientId: bigint
@@ -9,10 +11,12 @@ type PropsType = {
 const PatientDataVis = ({patientId}:PropsType) => {
     const {token} = useAuth()
     const bloodPressureService = new BloodPressureService(token)
+    const dosesService = new DosesService(token)
 
     return (
         <div className="graphsContainer">
             <SystoleGraph patientIdOrNegative={patientId} bloodPressureService={bloodPressureService}/>
+            <DoseGraph patientIdOrNegative={patientId} dosesService={dosesService}/>
         </div>
     )
 }
