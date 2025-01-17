722a0fcac26dc8dd3aff4c4b644cc07f54323e07
OBrien-Cathal <obrien.cathal@proton.me>
2025-01-17 19:01:21 +0100

    Fixed typo in AWS URL. Stage and dev builds now using correct backend url.

--- src/service/http-client.tsx
+++ src/service/http-client.tsx
@@ -6,9 +6,8 @@ export default axios.create({
   });
 
 export function baseUrl(): string {
-
-    // console.log(envURL.toString())
-    //
-    // let stringURL = 'http://localhost:8080/api/v1';
-    // console.log(stringURL)
-    return `${import.meta.env.VITE_API_URL}`;}
+    // let url = `${import.meta.env.VITE_API_URL}`;
+    // console.log(url)
+    // return url;
+    return `${import.meta.env.VITE_API_URL}`;
+}
