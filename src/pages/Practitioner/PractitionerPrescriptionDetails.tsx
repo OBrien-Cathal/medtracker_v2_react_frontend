cfb2ebca5665987438982b63dab9b674ec025c66
OBrien-Cathal <obrien.cathal@proton.me>
2025-01-19 18:05:34 +0100

    basic types and flow for saving a prescription

--- src/pages/Practitioner/PractitionerPrescriptionDetails.tsx
+++ src/pages/Practitioner/PractitionerPrescriptionDetails.tsx
@@ -1,8 +1,58 @@
-import { useParams} from "react-router-dom";
+import {useParams} from "react-router-dom";
 import {IParams} from "../../types/params.type.ts";
+import {useState} from "preact/compat";
+import {IPrescriptionDetailsType} from "../../types/prescription.type.ts";
+import {useEffect} from "react";
 
 const PractitionerPrescriptionDetails = () => {
     const {id, userId} = useParams<IParams>()
+    const prescriptionDetailsPlaceholder: IPrescriptionDetailsType = {
+        patientId: -1n,
+        baseDetails: {
+            id: -1n,
+            doseMg: 0,
+            medication: {id: -1n, name: ''},
+            practitionerUsername: '',
+            patientUsername: '',
+            beginTime: '',
+            endTime: ''
+        },
+        prescriptionScheduleEntries: []
+    }
+
+    const [prescription, setPrescription] = useState<IPrescriptionDetailsType>(prescriptionDetailsPlaceholder)
+
+    const [doseMg, setDoseMg] = useState(prescriptionDetailsPlaceholder.baseDetails.doseMg)
+
+    function getPrescriptionDetails() {
+        if (id && BigInt(id) < 0) return
+        console.log(`Getting details for prescription ID: ${id}`)
+
+        setPrescription(prescriptionDetailsPlaceholder)
+
+    }
+
+    function validatePrescriptionDetails(): boolean {
+        console.log(`Validating details for prescription ID: ${id}`)
+        console.log(prescription.patientId)
+        setPrescription(prescriptionDetailsPlaceholder)
+        return false
+    }
+
+    function savePrescriptionDetails() {
+        if (validatePrescriptionDetails())
+            console.log(`Saving details for prescription ID: ${id}`)
+        setPrescription(prescriptionDetailsPlaceholder)
+
+    }
+
+    useEffect(() => {
+        if (userId) {
+            prescriptionDetailsPlaceholder.patientId = BigInt(userId)
+        }
+        getPrescriptionDetails();
+    }, [])
+
 
     return (
         <div className="mainContainer">
@@ -11,6 +61,21 @@ const PractitionerPrescriptionDetails = () => {
             </div>
             <div>Fill PrescriptionDetails here for ID: {id}</div>
             <div> Patient ID: {userId}</div>
+            <div>
+                <input
+                    value={doseMg}
+                    type={'number'}
+                    placeholder='0'
+                    onChange={(ev) => setDoseMg(Number(ev.currentTarget.value))}
+                />
+
+            </div>
+            <div className={'prescription-actions'}>
+                <input className={'inputButton'} type='submit' value={'Save Prescription'} onClick={savePrescriptionDetails}/>
+
+            </div>
+
+
         </div>
     )
 }
--- src/types/prescription.type.ts
+++ src/types/prescription.type.ts
@@ -1,11 +1,27 @@
 import {IMedicationType} from "./medication.type.ts";
 
+
 export interface IPrescriptionType {
     id: bigint
     doseMg: number
     medication: IMedicationType
     patientUsername: string
-    practitionerUsername:string
+    practitionerUsername: string
     beginTime: string
-    endTime:string
+    endTime: string
+}
+
+export interface IPrescriptionDetailsType {
+    patientId: bigint
+    baseDetails: IPrescriptionType
+    prescriptionScheduleEntries: []
+}
+
+export interface IPrescriptionScheduleEntryType {
+    id: bigint
+    dayStage: string
+}
+
+export interface IPrescriptionUpdateRequestType {
+    prescriptionDetails: IPrescriptionDetailsType
 }
